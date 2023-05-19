da98c96590ed59c86f6198105a466508ae29077f
6utters <TheYomaLaughter@gmail.com>
2023-05-19 19:59:58 +0300

    basic styles & themization

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import {FC, Suspense} from 'react';
-import './index.scss'
+import {FC, Suspense, useContext, useState} from 'react';
 import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
+import './styles/index.scss'
 
 export const App: FC = () => {
+    const {theme, toggleTheme} = useTheme()
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
