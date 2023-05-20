0354edc59a76dfc4c11074032d3263a8dcccdd49
6utters <TheYomaLaughter@gmail.com>
2023-05-20 08:36:09 +0300

    widgets: Navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,17 @@
 import {FC} from 'react';
-import {Link} from "react-router-dom";
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
-import './styles/index.scss'
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
+import './styles/index.scss'
 
 export const App: FC = () => {
     const {theme, toggleTheme} = useTheme()
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>Toggle</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
+            <Navbar />
             <AppRouter/>
+            <button onClick={toggleTheme}>Toggle</button>
         </div>
     );
 };
