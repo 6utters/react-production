aa0255af5a244cba07b1a3e648361262fc463ba6
6utters <TheYomaLaughter@gmail.com>
2023-06-05 19:29:19 +0300

    shared: headlessUI ListBox

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,8 @@
 import { FC, memo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Page } from 'widgets/Page/ui/Page'
+import { HStack } from 'shared/ui/Stack'
+import { ListBox } from 'shared/ui/ListBox/ListBox'
 
 const MainPage: FC = () => {
   const { t } = useTranslation('main')
@@ -8,6 +10,27 @@ const MainPage: FC = () => {
   return (
     <Page>
       {t('Главная страница')}
+      <HStack>
+        <div>dasdas</div>
+        <ListBox
+          defaultValue="Choose Item"
+          value={undefined}
+          onChange={(value: string) => {}}
+          items={[
+            { value: '1', content: 'content 1' },
+            { value: '2', content: 'content 2', disabled: true },
+            { value: '3', content: 'content 3' }
+          ]}
+        />
+      </HStack>
+      <div>dasdas</div>
+      <div>dasdas</div>
+      <div>dasdas</div>
+      <div>dasdas</div>
+      <div>dasdas</div>
+      <div>dasdas</div>
+      <div>dasdas</div>
+      <div>dasdas</div>
     </Page>
   )
 }
