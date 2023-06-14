b5532c3a8d0e00532f14f30da6f7eb871673e596
6utters <TheYomaLaughter@gmail.com>
2023-06-14 19:27:27 +0300

    e2e tests: cypress basic config and tests

---
+++ cypress/e2e/common/routing.cy.ts
@@ -0,0 +1,31 @@
+import { selectByTestId } from '../helpers/selectByTestId'
+
+describe('Routing', () => {
+  describe('Authorized user', () => {
+    it('should go to main page', () => {
+      cy.visit('/')
+      cy.get(selectByTestId('MainPage')).should('exist')
+    })
+    it('should be redirected from profile page', () => {
+      cy.visit('/profile/1')
+      cy.get(selectByTestId('MainPage')).should('exist')
+    })
+    it('should be redirected from the page that doesn\'t exist', () => {
+      cy.visit('/random')
+      cy.get(selectByTestId('NotFoundPage')).should('exist')
+    })
+  })
+  describe('Unauthorized user', () => {
+    beforeEach(() => {
+      cy.login()
+    })
+    it('should go to profile page', () => {
+      cy.visit('/profile/1')
+      cy.get(selectByTestId('ProfilePage')).should('exist')
+    })
+    it('should go to articles page', () => {
+      cy.visit('/articles')
+      cy.get(selectByTestId('ArticlesPage')).should('exist')
+    })
+  })
+})
