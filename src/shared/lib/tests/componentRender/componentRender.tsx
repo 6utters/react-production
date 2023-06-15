a888e4febe3e7ed84b8a1d158d42a3e537281fd1
6utters <TheYomaLaughter@gmail.com>
2023-06-15 20:41:14 +0300

    e2e compomemt tests

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -5,23 +5,44 @@ import { MemoryRouter } from 'react-router-dom'
 import { ReducersMapObject } from '@reduxjs/toolkit'
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider'
 import i18nForTests from '../../../config/i18n/i18nForTests'
+// eslint-disable-next-line prod-path-plugin/layer-imports
+import { ThemeProvider } from '@/app/providers/ThemeProvider'
+import { Theme } from '@/shared/const/theme'
+// eslint-disable-next-line prod-path-plugin/layer-imports
+import '@/app/styles/index.scss'
 
 export interface componentRenderOptions {
   route?: string
   initialState?: DeepPartial<StateSchema>
   asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
+  theme?: Theme
 }
 
-export function componentRender(Component: ReactNode, options: componentRenderOptions = {}) {
-  const { route = '/', initialState, asyncReducers } = options
-  return render(
+interface TestProviderProps {
+  children: ReactNode
+  options?: componentRenderOptions
+}
+
+export function TestProvider(props: TestProviderProps) {
+  const { options = {}, children } = props
+  const {
+    route = '/', initialState, asyncReducers, theme = Theme.LIGHT
+  } = options
+  return (
     <MemoryRouter initialEntries={[route]}>
       <StoreProvider asyncReducers={asyncReducers} initialState={initialState}>
         <I18nextProvider i18n={i18nForTests}>
-          {Component}
+          <ThemeProvider initialTheme={theme}>
+            <div className={`app ${theme}`}>
+              {children}
+            </div>
+          </ThemeProvider>
         </I18nextProvider>
       </StoreProvider>
     </MemoryRouter>
-
   )
+}
+
+export function componentRender(Component: ReactNode, options: componentRenderOptions = {}) {
+  return render(<TestProvider options={options}>{Component}</TestProvider>)
 }
